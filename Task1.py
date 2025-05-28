0182800b82d63ead50dbb6d07ba0d3f2b1659daf
Athuajith <100676953+Athuajith@users.noreply.github.com>
2025-05-28 11:12:18 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,16 +1,7 @@
-value_1=int(input("Enter the first number"))
-value_2=int(input("Enter the second number"))
+number=int(input("Enter a number:"))
 
 
-
-def fn_calculate(value_1,value_2):
-    add_res=value_1 + value_2
-    sub_res=value_1 - value_2
-    mul_res=value_1 * value_2
-    div_res=value_1/value_2
-    print(f"Addition:{add_res}")
-    print(f"Subtraction:{sub_res}")
-    print(f"Multiplication:{mul_res}")
-    print(f"Division:{div_res}")
-
-fn_calculate(value_1,value_2)
+if(number % 2 == 0):
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
