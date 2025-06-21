733ff7c664f2e1198de1d28209169394f8abc41f
Athuajith <100676953+Athuajith@users.noreply.github.com>
2025-06-21 17:57:05 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,11 +1,28 @@
-def operation(number):
-    sq_root=math.sqrt(number)
-    log_num=math.log(number)
-    sine_num=math.sin(number)
+def edit_file(filename):
+   try:
+      msg = input("Enter text to write to the file:")
 
-    print(f"Square root: {sq_root}")
-    print(f"Logarithm: {log_num}")
-    print(f"Sine: {sine_num}")
+      file=open(filename,'w')
+      file.write(msg)
+      print(f"Data successfully written to {filename}")
+      file.close()
 
+      addmsg=input("Enter additional text to append:")
+      file=open(filename,'a')
+      file.write("\n")
+      file.write(addmsg)
+      file.close()
 
-operation(number)
+      print(f"File content of output text file is")
+      file=open(filename,'r')
+      fileMsg=file.read()
+      print(fileMsg)
+      file.close()
+
+   except FileNotFoundError:
+       print(f"Error: Could not find or open the file '{filename}'.")
+
+   except Exception as e:
+       print(f"An unexpected error occurred: {e}")
+
+edit_file("sample.txt")
