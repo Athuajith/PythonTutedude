a5aff4325397236c7378386110cfd8fa60ca26c6
Athuajith <100676953+Athuajith@users.noreply.github.com>
2025-06-24 19:39:17 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,28 +1,9 @@
-def edit_file(filename):
-   try:
-      msg = input("Enter text to write to the file:")
+list=[]
 
-      file=open(filename,'w')
-      file.write(msg)
-      print(f"Data successfully written to {filename}")
-      file.close()
-
-      addmsg=input("Enter additional text to append:")
-      file=open(filename,'a')
-      file.write("\n")
-      file.write(addmsg)
-      file.close()
-
-      print(f"File content of output text file is")
-      file=open(filename,'r')
-      fileMsg=file.read()
-      print(fileMsg)
-      file.close()
-
-   except FileNotFoundError:
-       print(f"Error: Could not find or open the file '{filename}'.")
-
-   except Exception as e:
-       print(f"An unexpected error occurred: {e}")
-
-edit_file("sample.txt")
+for i in range(1,11):
+    list.append(i)
+first_five=list[:5]
+rev_first_five=first_five[::-1]
+print(f"Original list:{list}")
+print(f"Extracted fist five elements:{first_five}")
+print(f"Reversed extracted five elements:{rev_first_five}")
