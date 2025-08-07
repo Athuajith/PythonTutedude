3804bb4d2247966d8082ee0de4f7cb2e27e837ff
Athuajith <100676953+Athuajith@users.noreply.github.com>
2025-08-07 17:28:54 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,9 +1,1 @@
-list=[]
 
-for i in range(1,11):
-    list.append(i)
-first_five=list[:5]
-rev_first_five=first_five[::-1]
-print(f"Original list:{list}")
-print(f"Extracted fist five elements:{first_five}")
-print(f"Reversed extracted five elements:{rev_first_five}")
